1a7e4d11dc7da08ed9563c7cb9fd792979b839e1
Jason Schweier <jason.schweier@gmail.com>
2021-11-07 10:41:28 -0500

    Day 3, part 2

--- lib/aoc/day_03_square_with_three_sides.ex
+++ lib/aoc/day_03_square_with_three_sides.ex
@@ -10,14 +10,35 @@ defmodule AoC.Day03SquareWithThreeSides do
   In a valid triangle, the sum of any two sides must be larger than the remaining side. For example, the "triangle" given above is impossible, because 5 + 10 is not larger than 25.
 
   In your puzzle input, how many of the listed triangles are possible?
+
+  --- Part Two ---
+
+  Now that you've helpfully marked up their design documents, it occurs to you that triangles are specified in groups of three vertically. Each set of three numbers in a column specifies a triangle. Rows are unrelated.
+
+  For example, given the following specification, numbers with the same hundreds digit would be part of the same triangle:
+
+  101 301 501
+  102 302 502
+  103 303 503
+  201 401 601
+  202 402 602
+  203 403 603
+
+  In your puzzle input, and instead reading by columns, how many of the listed triangles are possible?
   """
 
   def possible_triangles(input) do
     input
-    |> String.split("\n", trim: true)
-    |> Enum.map(fn str ->
-      str |> String.split(~r/\s+/, trim: true) |> Enum.map(&String.to_integer/1)
-    end)
+    |> numberize()
+    |> Enum.count(&possible_triangle?/1)
+  end
+
+  def possible_vertical_triangles(input) do
+    input
+    |> numberize()
+    |> Enum.chunk_every(3)
+    |> Enum.flat_map(&Enum.zip/1)
+    |> Enum.map(&Tuple.to_list/1)
     |> Enum.count(&possible_triangle?/1)
   end
 
@@ -25,5 +46,13 @@ defmodule AoC.Day03SquareWithThreeSides do
     [small, med, large] = Enum.sort(sides)
 
     small + med > large
+  end
+
+  defp numberize(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn str ->
+      str |> String.split(~r/\s+/, trim: true) |> Enum.map(&String.to_integer/1)
+    end)
   end
 end
--- lib/mix/tasks/solve.ex
+++ lib/mix/tasks/solve.ex
@@ -19,6 +19,7 @@ defmodule Mix.Tasks.Solve do
   defp solve("02", 2, input), do: AoC.Day02BathroomSecurity.bathroom_code(AoC.Day02BathroomSecurity.CrazyKeypad, input)
 
   defp solve("03", 1, input), do: AoC.Day03SquareWithThreeSides.possible_triangles(input)
+  defp solve("03", 2, input), do: AoC.Day03SquareWithThreeSides.possible_vertical_triangles(input)
 
   defp solve(_, _, _input), do: "not implemented!"
 end
