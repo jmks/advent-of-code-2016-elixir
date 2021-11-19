ee0ef9654b8c1c7a62961e6cfae7ab292156c351
Jason Schweier <jason.schweier@gmail.com>
2021-11-19 17:10:10 -0500

    Day 14, part 2

--- lib/aoc/day_14_otp.ex
+++ lib/aoc/day_14_otp.ex
@@ -20,37 +20,98 @@ defmodule AoC.Day14OTP do
   So, using our example salt of abc, index 22728 produces the 64th key.
 
   Given the actual salt in your puzzle input, what index produces your 64th one-time pad key?
-  """
-  def generate_keys(salt, count \\ 64), do: do_generate_keys(salt, 0, count, [])
 
-  def nth_key_index(salt, n) do
+  --- Part Two ---
+
+  Of course, in order to make this process even more secure, you've also implemented key stretching.
+
+  Key stretching forces attackers to spend more time generating hashes. Unfortunately, it forces everyone else to spend more time, too.
+
+  To implement key stretching, whenever you generate a hash, before you use it, you first find the MD5 hash of that hash, then the MD5 hash of that hash, and so on, a total of 2016 additional hashings. Always use lowercase hexadecimal representations of hashes.
+
+  For example, to find the stretched hash for index 0 and salt abc:
+
+  Find the MD5 hash of abc0: 577571be4de9dcce85a041ba0410f29f.
+  Then, find the MD5 hash of that hash: eec80a0c92dc8a0777c619d9bb51e910.
+  Then, find the MD5 hash of that hash: 16062ce768787384c81fe17a7a60c7e3.
+  ...repeat many times...
+  Then, find the MD5 hash of that hash: a107ff634856bb300138cac6568c0f24.
+  So, the stretched hash for index 0 in this situation is a107ff.... In the end, you find the original hash (one use of MD5), then find the hash-of-the-previous-hash 2016 times, for a total of 2017 uses of MD5.
+
+  The rest of the process remains the same, but now the keys are entirely different. Again for salt abc:
+
+  The first triple (222, at index 5) has no matching 22222 in the next thousand hashes.
+  The second triple (eee, at index 10) hash a matching eeeee at index 89, and so it is the first key.
+  Eventually, index 22551 produces the 64th key (triple fff with matching fffff at index 22859.
+
+  Given the actual salt in your puzzle input and using 2016 extra MD5 calls of key stretching, what index now produces your 64th one-time pad key?
+  """
+  defmodule MD5Hasher do
+    use Agent
+
+    def start_link(stretches) do
+      Agent.start_link(fn -> {stretches, %{}} end)
+    end
+
+    def hash(pid, string, index) do
+      hash(pid, string <> Integer.to_string(index))
+    end
+
+    def hash(pid, string) do
+      {stretches, cache} = Agent.get(pid, fn state -> state end)
+
+      if Map.has_key?(cache, string) do
+        Map.fetch!(cache, string)
+      else
+        hashed = md5(string, stretches)
+
+        Agent.update(pid, fn _ -> {stretches, Map.put_new(cache, string, hashed)} end)
+
+        hashed
+      end
+    end
+
+    defp md5(string, stretches) do
+      hashed = :crypto.hash(:md5, string) |> Base.encode16(case: :lower)
+
+      case stretches do
+        0 -> hashed
+        _ -> md5(hashed, stretches - 1)
+      end
+    end
+  end
+
+  def generate_keys(salt, count \\ 64, stretches \\ 0) do
+    {:ok, hasher} = MD5Hasher.start_link(stretches)
+
+    do_generate_keys(salt, 0, hasher, count, [])
+  end
+
+  def nth_key_index(salt, n, stretches \\ 0) do
     salt
-    |> generate_keys(n)
+    |> generate_keys(n, stretches)
     |> List.last()
     |> elem(0)
   end
 
-  def do_generate_keys(_salt, _index, count, keys) when length(keys) == count, do: Enum.reverse(keys)
+  def do_generate_keys(_salt, _index, _hasher, count, keys) when length(keys) == count, do: Enum.reverse(keys)
 
-  def do_generate_keys(salt, index, count, keys) do
-    hash = md5(salt, index)
+  def do_generate_keys(salt, index, hasher, count, keys) do
+    hash = MD5Hasher.hash(hasher, salt, index)
 
     case triplet(hash) do
       {:ok, char} ->
         target = List.duplicate(char, 5) |> Enum.join()
-        if next_hashes_include_repeated?(target, salt, index + 1, 1_000) do
-          do_generate_keys(salt, index + 1, count, [{index, hash} | keys])
+
+        if next_hashes_include_repeated?(target, salt, index + 1, hasher, 1_000) do
+          do_generate_keys(salt, index + 1, hasher, count, [{index, hash} | keys])
         else
-          do_generate_keys(salt, index + 1, count, keys)
+          do_generate_keys(salt, index + 1, hasher, count, keys)
         end
 
       :error ->
-        do_generate_keys(salt, index + 1, count, keys)
+        do_generate_keys(salt, index + 1, hasher, count, keys)
     end
-  end
-
-  defp md5(salt, index) do
-    :crypto.hash(:md5, salt <> Integer.to_string(index)) |> Base.encode16(case: :lower)
   end
 
   defp triplet(string) do
@@ -63,15 +124,15 @@ defmodule AoC.Day14OTP do
   defp do_triplet([a, a, a | _]), do: {:ok, a}
   defp do_triplet([_ | rest]), do: do_triplet(rest)
 
-  defp next_hashes_include_repeated?(_needle, _salt, _index, 0), do: false
+  defp next_hashes_include_repeated?(_needle, _salt, _hasher, _index, 0), do: false
 
-  defp next_hashes_include_repeated?(needle, salt, index, hashes) do
-    hash = md5(salt, index)
+  defp next_hashes_include_repeated?(needle, salt, index, hasher, hashes) do
+    hash = MD5Hasher.hash(hasher, salt, index)
 
     if String.contains?(hash, needle) do
       true
     else
-      next_hashes_include_repeated?(needle, salt, index + 1, hashes - 1)
+      next_hashes_include_repeated?(needle, salt, index + 1, hasher, hashes - 1)
     end
   end
 end
--- test/aoc/day_14_test.exs
+++ test/aoc/day_14_test.exs
@@ -8,9 +8,15 @@ defmodule AoC.Day14Test do
       {39, "347dac6ee8eeea4652c7476d0f97bee5"},
       {92, "ae2e85dd75d63e916a525df95e999ea0"}
     ]
+
+    assert generate_keys("abc", 1, 2016) == [
+      {10, "4a81e578d9f43511ab693eee1a75f194"}
+    ]
   end
 
+  # Warning: slow...
   test "nth_key_index" do
     assert nth_key_index("abc", 64) == 22728
+    assert nth_key_index("abc", 64, 2016) == 22551
   end
 end
