c592cfb05a145456673a9aed1dd5d581b1108219
Jason Schweier <jason.schweier@gmail.com>
2021-11-24 18:04:58 -0500

    Day 20, part 2

--- lib/aoc/day_20_firewall.ex
+++ lib/aoc/day_20_firewall.ex
@@ -15,6 +15,10 @@ defmodule AoC.Day20Firewall do
   The blacklist specifies ranges of IPs (inclusive of both the start and end value) that are not allowed. Then, the only IPs that this firewall allows are 3 and 9, since those are the only numbers not in any range.
 
   Given the list of blocked IPs you retrieved from the firewall (your puzzle input), what is the lowest-valued IP that is not blocked?
+
+  --- Part Two ---
+
+  How many IPs are allowed by the blacklist?
   """
   defmodule Parser do
     import NimbleParsec
@@ -27,18 +31,45 @@ defmodule AoC.Day20Firewall do
 
   def lowest_allowed(rules) do
     rules
+    |> sorted_rules()
+    |> lowest(0)
+  end
+
+  def ips_allowed(rules, max) do
+    rules
+    |> sorted_rules()
+    |> count(0, max, 0)
+  end
+
+  defp sorted_rules(rules) do
+    rules
     |> String.split("\n", trim: true)
     |> Enum.map(&Parser.ip_range/1)
     |> Enum.map(fn {:ok, [min, max], _, _, _, _} ->
       {min, max}
     end)
     |> Enum.sort_by(fn {min, _max} -> min end)
-    |> lowest(0)
   end
 
   defp lowest([{min, _max} | _rest], low) when low < min, do: low
 
-  defp lowest([{min, max} | rest], low) do
+  defp lowest([{_min, max} | rest], low) do
     lowest(rest, Enum.max([low, max + 1]))
+  end
+
+  defp count([], ip, maximum, total) do
+    if ip > maximum do
+      total
+    else
+      maximum - ip + 1 + total
+    end
+  end
+
+  defp count(rules = [{min, max} | rest], ip, maximum, total) when ip < min do
+    count(rules, ip + 1, maximum, total + 1)
+  end
+
+  defp count([{_min, max} | rest], ip, maximum, total) do
+    count(rest, Enum.max([ip, max + 1]), maximum, total)
   end
 end
--- test/aoc/day_20_test.exs
+++ test/aoc/day_20_test.exs
@@ -4,11 +4,18 @@ defmodule AoC.Day20Test do
   import AoC.Day20Firewall
 
   test "lowest_allowed" do
-    # 5721363
     assert lowest_allowed("""
            5-8
            0-2
            4-7
            """) == 3
+  end
+
+  test "ips_allowed" do
+    assert ips_allowed("""
+    5-8
+    0-2
+    4-7
+    """, 9) == 2
   end
 end
